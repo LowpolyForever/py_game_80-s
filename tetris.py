3e97507f7b7536d74f3f2ab47dcbeb916f1d2bc3
LowpolyForever <62081209+LowpolyForever@users.noreply.github.com>
2020-03-12 11:51:05 +1100

    hjbhbjbj

--- tetris.py
+++ tetris.py
@@ -3,8 +3,32 @@ import math
 
 pygame.init()
 
-print("helloworld")
+winx = 200
+winy = 200
 
-set = input("type")
+cx = 100
+cy = 100
 
-print(set)
+delay = 30
+
+win = pygame.display.set_mode((winx,winy))
+pygame.display.set_caption("TETRIS")
+
+run = True
+
+while run:
+    pygame.time.delay(delay)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        
+
+    win.fill((0,0,0))  # Fills the screen with black
+    pygame.draw.circle(win, (255,255,255), (cx,cy), (10)) #circle
+    
+    pygame.display.flip()
+
+    
+pygame.quit()
